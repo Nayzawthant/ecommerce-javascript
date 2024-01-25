a650fcff03031d55410c47aac876585219b416f1
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2024-01-25 21:29:32 +0630

    ecommerce-javascript

---
+++ main.js
@@ -0,0 +1,51 @@
+// Products Array
+const products = [
+    {
+        id: 1,
+        title: "Autumn Hoodie",
+        price: 264.9,
+        image: ""
+    },
+    {
+        id: 2,
+        title: "FUSHION HOODIE",
+        price: 295,
+        image: ""
+    },
+    {
+        id: 3,
+        title: "Chestnut Brown",
+        price: 74.9,
+        image: ""
+    },
+    {
+        id: 4,
+        title: "Nike Sportswear",
+        price: 80,
+        image: ""
+    },
+    {
+        id: 5,
+        title: "Champion BASIC",
+        price: 48.99,
+        image: ""
+    },
+    {
+        id: 6,
+        title: "Cotton Hoodie",
+        price: 395,
+        image: ""
+    },
+    {
+        id: 7,
+        title: "CLASSIC CREWNECK",
+        price: 48.99,
+        image: ""
+    },
+    {
+        id: 8,
+        title: "THPE HOODED",
+        price: 79.99,
+        image: ""
+    },
+]
